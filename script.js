efe294405b4f52854a79de1973c1edf12b33e3f8
pablo <4053835@alu.murciaeduca.es>
2024-11-08 10:09:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+
+const ctx = canvas.getContext("2d");
+
+//Ancho y largo de la pantalla
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+//creamos la bola
+
+let bolas = []; 
+for (let i=0; i<10; i++){
+    bolas.push(new Bola(canvas.width/2, canvas.height/2)); 
+}
+
+
+
+function animar() {
+
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+
+    bolas.forEach(bola =>{
+        bola.dibujar(); 
+        bola.mover(); 
+    })
+
+    requestAnimationFrame(animar);
+}
+animar(); 
